cfd680a948f3fb311e4683e715f0d864a02b187f
nana kwadwo <nampong32@gmail.com>
2025-02-04 06:50:01 -0500

    added tf.vars file

---
+++ iam/variable.tf
@@ -0,0 +1,11 @@
+variable "user_name" {
+  default = "Jack"
+  type = string
+  description = "this is the default name"
+}
+
+variable "region" {
+  default = "us-east-1"
+  type = string
+  description = "this is default region"
+}
